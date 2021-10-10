15ab91a5a00b968cc238653db57258c8d3d2f1c8
wry <lzavaleta@gmail.com>
2021-10-10 14:18:53 +0200

    vidgen - Remove optimizations, remove {} init, so no implicit memset used

--- vidgen/src/main.c
+++ vidgen/src/main.c
@@ -101,7 +101,8 @@ void start(void)
     float col[3];
     float col2[3];
 
-    unsigned char coli[] = { 0, 0, 0 };
+    unsigned char coli[3];
+    coli[0] = coli[1] = coli[2] = 0;
 
     for (int y=0; y < h; y++)
     {
