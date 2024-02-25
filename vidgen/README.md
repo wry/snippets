66e9e3bba887bf2d71ca1fb7f5a92d7d8ed8615d
wry <lzavaleta@gmail.com>
2024-02-25 17:33:42 +0100

    vidgen: Make it run on arm64

--- vidgen/README.md
+++ vidgen/README.md
@@ -1,2 +1,4 @@
 # vidgen
-a tiny nostdlib animation using ffmpeg as its gui
+a tiny macos nostdlib animation backed by x87 asm, using ffplay as its display
+
+(on arm64 it will run but uses libm)
--- vidgen/src/main.c
+++ vidgen/src/main.c
@@ -9,9 +9,15 @@
 #error "The only supported platform for now is macos"
 #endif /* !defined(__APPLE__) || !defined(__MACH__) */
 
-#define PI 3.14159265359
+#ifdef __aarch64__
+#  include <math.h>
+#endif
 
-static int write(int fd, const void * buf, unsigned long buflen)
+#define PI 3.14159265359
+#define PI_2 (PI * 0.5)
+#define PI_MUL_2 (PI * 2.0)
+
+static int vg_write(int fd, const void * buf, unsigned long buflen)
 {
   int ret;
 #if defined(__x86_64__)
@@ -32,49 +38,7 @@ static int write(int fd, const void * buf, unsigned long buflen)
   return ret;
 }
 
-static float fsin(float r)
-{
-  float ret;
-#if defined(__x86_64__)
-  asm (
-    "fsin"
-    : "=t" (ret)
-    : "0" (r)
-  );
-#elif defined(__aarch64__)
-#endif
-  return ret;
-}
-
-static float fcos(float r)
-{
-  float ret;
-#if defined(__x86_64__)
-  asm (
-    "fcos"
-    : "=t" (ret)
-    : "0" (r)
-  );
-#elif defined(__aarch64__)
-#endif
-  return ret;
-}
-
-static float fsqrt(float v)
-{
-  float ret;
-#if defined(__x86_64__)
-  asm (
-    "fsqrt"
-    : "=t" (ret)
-    : "0" (v)
-  );
-#elif defined(__aarch64__)
-#endif
-  return ret;
-}
-
-static float fabs(float v)
+static float vg_fabs(float v)
 {
   float ret;
 #if defined(__x86_64__)
@@ -84,11 +48,80 @@ static float fabs(float v)
     : "0" (v)
   );
 #elif defined(__aarch64__)
+  ret = v < 0.0 ? -v : v;
 #endif
   return ret;
 }
 
-static double fpatan(float y, float x)
+#if defined(__aarch64__)
+static float vg_fmod(float a, float b)
+{
+  return a-b * (int)(a/b);
+}
+
+float clamp(float x)
+{
+  return x < 0.0 ? 0.0 : (x > 1.0 ? 1.0 : x);
+}
+
+float smoothstep(float edge0, float edge1, float x)
+{
+  x = clamp((x - edge0) / (edge1 - edge0));
+  return x*x*(3.0-2.0*x);
+}
+
+float tri(float x)
+{
+  return vg_fabs(vg_fmod(x, PI*2.0) - (float)PI) / PI;
+}
+#endif
+
+static float vg_fsin(float r)
+{
+  float ret;
+#if defined(__x86_64__)
+  asm (
+    "fsin"
+    : "=t" (ret)
+    : "0" (r)
+  );
+#elif defined(__aarch64__)
+  ret = smoothstep(0.0, 1.0, tri(r+PI_2*3)) * 2.0 - 1.0;
+#endif
+  return ret;
+}
+
+static float vg_fcos(float r)
+{
+  float ret;
+#if defined(__x86_64__)
+  asm (
+    "fcos"
+    : "=t" (ret)
+    : "0" (r)
+  );
+#elif defined(__aarch64__)
+  ret = vg_fsin(PI_2 - r);
+#endif
+  return ret;
+}
+
+static float vg_fsqrt(float v)
+{
+  float ret;
+#if defined(__x86_64__)
+  asm (
+    "fsqrt"
+    : "=t" (ret)
+    : "0" (v)
+  );
+#elif defined(__aarch64__)
+  ret = sqrtf(v);
+#endif
+  return ret;
+}
+
+static double vg_fpatan(float y, float x)
 { 
   float ret; 
 #if defined(__x86_64__)
@@ -98,6 +131,7 @@ static double fpatan(float y, float x)
     : "0" (x), "u" (y)
   );   
 #elif defined(__aarch64__)
+  ret = atan2f(y, x);
 #endif
   return ret; 
 }
@@ -111,14 +145,14 @@ static void rgb2yuv444(unsigned char rgb[3], unsigned char yuv[3])
 
 static float vec2length(float v[2])
 {
-  return fsqrt(v[0] * v[0] + v[1] * v[1]);
+  return vg_fsqrt(v[0] * v[0] + v[1] * v[1]);
 }
 
 static void vec3mix(float dst[3], float v1[3], float v2[3], float a)
 {
-  dst[0] = fsqrt(v1[0] * v1[0] * (1.0f - a) + v2[0] * v2[0] * a);
-  dst[1] = fsqrt(v1[1] * v1[1] * (1.0f - a) + v2[1] * v2[1] * a);
-  dst[2] = fsqrt(v1[2] * v1[2] * (1.0f - a) + v2[2] * v2[2] * a);
+  dst[0] = v1[0] * v1[0] * (1.0f - a) + v2[0] * v2[0] * a;
+  dst[1] = v1[1] * v1[1] * (1.0f - a) + v2[1] * v2[1] * a;
+  dst[2] = v1[2] * v1[2] * (1.0f - a) + v2[2] * v2[2] * a;
 }
 
 void start(void)
@@ -133,11 +167,11 @@ void start(void)
 
   float t, r, uv[2];
 
-  write(1, "YUV4MPEG2 W256 H256 F30:1 Ip A0:0 C444 XYSCSS=444\n", 50);
+  vg_write(1, "YUV4MPEG2 W256 H256 F30:1 Ip A0:0 C444 XYSCSS=444\n", 50);
 
   while (1)
   {
-    write(1, "FRAME\n", 6);
+    vg_write(1, "FRAME\n", 6);
 
     t = (float)f / 30.0f;
 
@@ -157,18 +191,18 @@ void start(void)
 
         col2[0] = 0.9f, col2[1] = 0.3f, col2[2] = 0.3f;
         r = vec2length(uv) < 0.5f ? 1.0f : 0.0f;
-        r *= PI*fsin(t) < fpatan(uv[1], uv[0]) ? 0.0f : 1.0f;
+        r *= PI*vg_fsin(t) < vg_fpatan(uv[1], uv[0]) ? 0.0f : 1.0f;
         r *= vec2length(uv) > 0.25f ? 1.0f : 0.0f;
         vec3mix(col, col, col2, r);
 
         col2[0] = 0.3f, col2[1] = 0.8f, col2[2] = 0.3f;
         r = vec2length(uv) < 0.35f ? 1.0f : 0.0f;;
-        r *= fpatan(uv[1], uv[0]) > PI*fcos(t) ? 1.0f : 0.0f;
+        r *= vg_fpatan(uv[1], uv[0]) > PI*vg_fcos(t) ? 1.0f : 0.0f;
         vec3mix(col, col, col2, r);
 
-        coli[0] = (unsigned char)(256.0f * col[0]);
-        coli[1] = (unsigned char)(256.0f * col[1]);
-        coli[2] = (unsigned char)(256.0f * col[2]);
+        coli[0] = (unsigned char)(255.0f * col[0]);
+        coli[1] = (unsigned char)(255.0f * col[1]);
+        coli[2] = (unsigned char)(255.0f * col[2]);
 
         rgb2yuv444(coli, yuv);
 
@@ -178,7 +212,7 @@ void start(void)
       }
     }
 
-    write(1, buf, res * 3);
+    vg_write(1, buf, res * 3);
 
     f++;
   }
