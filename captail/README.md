9851a6c757985707a1c8fe1a9cc14de6cabf61f5
wry <lzavaleta@gmail.com>
2024-03-09 10:48:49 +0100

    captail: Add ascii output, update readme

--- captail/README.md
+++ captail/README.md
@@ -1,14 +1,17 @@
 # captail
 
-a simple tool to extract bytes from a pcap stream on stdin
-where each row represents a single packet
+a simple tool to extract bytes from a pcap stream on stdin where each row represents a single packet
 
-use :
+## use
+
+$0 [\<format\>]
 
 * $0 0
   * hex string encoded bytes
 * $0 1
   * comma separated uint8\_t values for inclusion in code
+* $0 2
+  * ascii output
 
 ## examples
 
@@ -21,12 +24,11 @@ tcpdump -X -v -r capture.pcap | captail.pl
 ### extract bytes from live stream
 
 ```shell
-tcpdump -i en0 -s 0 -X -v | captail.pl
+tcpdump -X -v -i en0 -s 0 | captail.pl
 ```
 
-### convert output to bytestream and pass it to hexdump/nc/etc
+### extract bytes from live stream, convert them to binary data and pass it to hexdump (or nc/ncat/socat/etc)
 
 ```shell
-tcpdump -i en0 -s 0 -X -v | captail.pl | perl -pe 'chomp;$_=pack"H*",$_' | hexdump -C
+tcpdump -X -v -i en0 -s 0 | captail.pl | perl -pe 'chomp;$_=pack"H*",$_' | hexdump -C
 ```
-
--- captail/captail.pl
+++ captail/captail.pl
@@ -1,26 +1,42 @@
 #!/usr/bin/env perl
+#
+#  Copyright (c) 2024 wry
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
 
-# extract bytes from a tcpdump packet capture stream (for code or for further processing)
-# use : tcpdump -X -v ... | $0 [0|1]
+# extract bytes from a tcpdump packet capture stream
+# use : tcpdump -X -v ... | $0 [0|1|2]
 
 use strict;
 use warnings;
 
-my $argCode = 0;
-
-if ($ARGV[0]) {
-  $argCode = $ARGV[0] eq "1";
-}
+my $argFmt = $ARGV[0] || 0;
 
 my $buf = "";
-my $buflen = 0;
+my $buflen = -1;
 while(<STDIN>) {
   my $tmpbuf;
   if ($_=~/^\d\d:\d\d:\d\d.+length\s(\d+).*$/) {
     $buf = "";
     $buflen = $1;
   }
-  elsif ($_=~/^\s+0x\d{4}:(.+)\s\s.+/) {
+  elsif ($_=~/^\s+0x[0-9a-f]{4}:(.+)\s\s.+/) {
     $tmpbuf = $1;
     $tmpbuf =~ s/\s//g;
   }
@@ -30,7 +46,14 @@ while(<STDIN>) {
   }
 
   if (length($buf) == $buflen*2) {
-    print $argCode ? join(",", map{"0x$_"} ($buf=~m/../g))."\n" : "$buf\n";
+    if ($argFmt == 1) {
+      print join(",", map{"0x".$_} ($buf=~m/../g))."\n";
+    }
+    elsif ($argFmt == 2) {
+      print join("", map{my $b=hex($_); $b > 0x1f && $b < 0x7f ? chr($b) : '.'} ($buf=~m/../g))."\n";
+    }
+    else {
+      print "$buf\n";
+    }
   }
 }
-
